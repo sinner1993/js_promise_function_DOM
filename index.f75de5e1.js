8591eaf59aa6d80fbfce4a406cd47a586a012da0
sinner1993 <sergio1993goga@gmail.com>
2024-09-15 20:30:27 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,29 @@
+"use strict";
+function waitFor(element, eventName) {
+    return new Promise((resolve, reject)=>{
+        element.addEventListener(eventName, (e)=>{
+            e.preventDefault();
+            resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}. `);
+        });
+    });
+}
+const printMessage = (message)=>{
+    const body = document.querySelector("body");
+    const div = document.createElement("div");
+    div.setAttribute("class", "message");
+    div.textContent = message;
+    body.append(div);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
